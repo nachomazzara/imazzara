3ae45e71b51509469101c0612104c07bc331d0d3
Nacho <nachomazzara@gmail.com>
2026-01-08 19:47:23 -0300

    feat: update helmet

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -130,7 +130,8 @@ function BlogPost() {
     )
   }
 
-  const title = `${post.title} | Nacho Mazzara`
+  const title = post.title
+  const fullTitle = `${post.title} | Nacho Mazzara`
   const url = `https://imazzara.com/blog/${slug}`
   const imageUrl = post.ogImage 
     ? `https://imazzara.com${post.ogImage}`
@@ -139,18 +140,30 @@ function BlogPost() {
   return (
     <>
       <Helmet>
-        <title>{title}</title>
+        <title>{fullTitle}</title>
         <meta name="title" content={title} />
         <meta name="description" content={description} />
+        <link rel="canonical" href={url} />
+        
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content="article" />
         <meta property="og:image" content={imageUrl} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:site_name" content="Nacho Mazzara" />
+        
+        {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={url} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={imageUrl} />
+        <meta name="twitter:site" content="@nachomazzara" />
+        <meta name="twitter:creator" content="@nachomazzara" />
       </Helmet>
       <div className="blog-container">
         {post.hero && (
